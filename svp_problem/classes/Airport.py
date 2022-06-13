c514490ef54b7b33381b3fba1940bca1d5221f47
eric <ericdominguezm@gmail.com>
2022-06-13 12:22:55 +0200

    fixed a TOC of tabs

--- svp_problem/classes/Airport.py
+++ svp_problem/classes/Airport.py
@@ -5,16 +5,16 @@ class Airport():
         self.name = name
         self.dests_in_scope = dests_in_scope
         self.dest_map = dest_map
-      
+        
     def get_name(self):
         return self.name
-      
+        
     def get_dests_in_scope(self):
         return self.dests_in_scope
-      
+        
     def get_dest_map(self):
         return self.dest_map
-
+        
     def print_info(self):
         print(self.name)
         print(self.dests_in_scope)
