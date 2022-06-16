4d937e61550733ecacb7bf460acb3cb373068f59
Éric <60858791+DoMo-98@users.noreply.github.com>
2022-06-16 17:35:47 +0000

    reorganized the Airport class and added exit option and show data option into app.py

--- svp_problem/app.py
+++ svp_problem/app.py
@@ -9,13 +9,21 @@ def run():
     """
     airports_list = [] if read_data( 'airports_list.obj' ) is None else read_data( 'airports_list.obj' )
     # dest_map = {} if read_data( 'dest_map.obj' ) is None else read_data( 'dest_map.obj' )
-    print( 'airports_list: ' + str( airports_list ) )
+    show_data( airports_list )
+    # print( 'airports_list: ' + str( airports_list ) )
     # print( 'dest_map: ' + str( dest_map ) )
-    print( '\n' )
 
-    # TODO add show data option; add option to update dest_map.obj; add option to show the destination route
-    option = input("Choose your option: \n1. Create new airport\n")
-    if option == '1':
-        airport = Airport( input("Airport name: "), input("Airport destinations: ").split(' ') )
-        airports_list.append( airport )
-        write_data( airports_list, 'airports_list.obj' )
+    option = -1
+
+    while option != 0:
+        # TODO add show data option; add option to update dest_map.obj; add option to show the destination route
+        option = int( input("\nChoose your option: \n1. Create new airport\n2. Show airports data\n0. Exit\n") )
+
+        if option == 0:
+            return
+        if option == 1:
+            airport = Airport( input("Airport name: "), input("Airport destinations: ").split(' ') )
+            airports_list.append( airport )
+            write_data( airports_list, 'airports_list.obj' )
+        if option == 2:
+            show_data( airports_list )
--- svp_problem/classes/Airport.py
+++ svp_problem/classes/Airport.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# TODO change the flux of the program, its better if the airports_list is a dictionary with the airport name as key
 class Airport():
-    def __init__(self, name, dests_in_scope, dest_map = {}):
+    def __init__( self, name, dests_in_scope, best_route_map = {} ):
         self.name = name
         self.dests_in_scope = dests_in_scope
-        self.dest_map = dest_map
+        self.best_route_map = best_route_map
         
-    def get_name(self):
+    def get_name( self ):
         return self.name
         
-    def get_dests_in_scope(self):
+    def get_dests_in_scope( self ):
         return self.dests_in_scope
         
-    def get_dest_map(self):
-        return self.dest_map
+    def get_dest_map( self ):
+        return self.best_route_map
         
-    def print_info(self):
-        print(self.name)
-        print(self.dests_in_scope)
-        print(self.dest_map)
-        print("")
+    def print_info( self ):
+        print( "\n" + self.name )
+        print( self.dests_in_scope )
+        print( self.best_route_map )
+
+    def add_dest( self, dest ):
+        self.dests_in_scope.append( dest )
+
+    def add_best_route( self, dest, best_route ):
+        self.best_route_map[ dest ] = best_route
